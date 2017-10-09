4fa64db80a6263b0b7410ad1d739c8235cdea0dc
wulfric02 <adithya040191@gmail.com>
2017-10-09 15:58:10 +0530

    weather api integration

--- index.js
+++ index.js
@@ -6,14 +6,71 @@ var app = express()
 
 app.use(bodyParser.json())
 app.listen('4567')
+
 app.get('/', function (req, res) {
-res.send('helo')
+  res.send('helo')
 })
-app.post('/', function (req, res) {
-console.log(req.body)
-let response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-res.send(JSON.stringify({ "speech": response, "displayText": response
-        //"speech" is the spoken version of the response, "displayText" is the visual version
-}));
-})
+
+/*app.post('/', function (req, res) {
+  console.log(req.body)
+  let response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+  res.setHeader('Content-Type', 'application/json') //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response}))
+})*/
+
+const http = require('http');
+const host = 'api.worldweatheronline.com';
+const wwoApiKey = process.env.weatherkey;
+app.post('/', (req, res) => {
+  // Get the city and date from the request
+  let city = req.body.result.parameters['geo-city']; // city is a required param
+  // Get the date for the weather forecast (if present)
+  let date = '';
+  if (req.body.result.parameters['date']) {
+    date = req.body.result.parameters['date'];
+    console.log('Date: ' + date);
+  }
+  // Call the weather API
+  callWeatherApi(city, date).then((output) => {
+    // Return the results of the weather API to API.AI
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
+  }).catch((error) => {
+    // If there is an error let the user know
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+  });
+};
+function callWeatherApi (city, date) {
+  return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
+    console.log('API Request: ' + host + path);
+    // Make the HTTP request to get the weather
+    http.get({host: host, path: path}, (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let forecast = response['data']['weather'][0];
+        let location = response['data']['request'][0];
+        let conditions = response['data']['current_condition'][0];
+        let currentConditions = conditions['weatherDesc'][0]['value'];
+        // Create response
+        let output = `Current conditions in the ${location['type']} 
+        ${location['query']} are ${currentConditions} with a projected high of
+        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+        ${forecast['date']}.`;
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        reject(error);
+      });
+    });
+  });
+}
